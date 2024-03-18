15dc89741aeafe6e72da076df6023c087ef60d27
Arianna Grassi <ariannagrassi12@gmail.com>
2024-03-18 14:26:24 +0100

    prima rilascio progetto

---
+++ sql/setup.sql
@@ -0,0 +1,20 @@
+drop table if exists affinity;
+
+-- creazione tabella Users
+create table users (
+users_id serial primary key, 
+name varchar(20) not null,
+surname varchar(20) not null,
+birth_date date not null,
+gender char not null,
+email varchar(40) not null,
+password varchar(40) not null
+);
+
+create table affinity (
+affinity_id serial primary key,
+users_id integer unique,
+smoker char check(smoker in ('Y','N')),
+traveler char check(traveler in ('Y','N')),
+sporty char check(sporty in ('Y','N'))
+);
